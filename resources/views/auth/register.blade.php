48fd10b122ce0a02be2f20eefad96ba3a1aa5b4b
celtre <t00040901@utbvirtual.edu.co>
2015-10-19 14:41:44 -0500

    Push #3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,40 @@
 				<div class="panel-body">
 					@include('partials/errors')
 
+					<body class="login-img3-body">
+
+						<div class="container">
+
+							<form class="login-form" method="POST" action="{{ route('register') }}">
+								<div class="login-wrap">
+										<p class="login-img"><i class="icon_lock_alt"></i></p>
+										<div class="input-group">
+											<span class="input-group-addon"><i class="icon_profile"></i></span>
+											<input type="text" class="form-control" name="name" placeholder="Username" autofocus>
+										</div>
+										<div class="input-group">
+											<span class="input-group-addon"><i class="icon_profile"></i></span>
+											<input type="email" class="form-control" name="email" placeholder="Email">
+										</div>
+										<div class="input-group">
+												<span class="input-group-addon"><i class="icon_key_alt"></i></span>
+												<input type="password" class="form-control" name="password"  placeholder="Password">
+										</div>
+										<div class="input-group">
+											<span class="input-group-addon"><i class="icon_key_alt"></i></span>
+											<input type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password">
+										</div>
+
+										<button class="btn btn-primary btn-lg btn-block" type="submit">send</button>
+
+
+								</div>
+							</form>
+
+						</div>
+
+
+					</body>
 					<form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
